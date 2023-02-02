9c0921b8d4e427707eaf96fce7b9a446ef0880f6
Daniel Manjabacas <dannymm12@hotmail.com>
2023-02-02 20:50:23 +0000

    Ver 1.0

--- src/app.js
+++ src/app.js
@@ -7,5 +7,128 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let avaiblePronoums = ["ese", "esa", "el", "la", "un", "una"];
+  let avaibleAdjetives = [
+    "increible",
+    "elegante",
+    "adorable",
+    "impaciente",
+    "imparable"
+  ];
+  let avaibleExtensions = [".com", ".es", ".nz", ".edu", ".mx", ".net"];
+
+  // Mensajes de alerta
+
+  let ALERT_MISSING_CHECKBOX = `
+  <div class="alert alert-danger" role="alert">
+    POR FAVOR MARQUE AL MENOS UNA CASILLA DE CADA SECCION
+  </div>
+  `;
+
+  let ALERT_MISSING_NAME = `
+  <div class="alert alert-danger" role="alert">
+    POR FAVOR INTRODUZCA AL MENOS UN NOMBRE (LOS ESPACIOS SERAN ELIMINADOS AUTOMATICAMENTE).
+  </div>
+  `;
+
+  // Listener del boton
+  let rawForm = document.getElementById("formOptions");
+  rawForm.addEventListener("submit", recibirDatosForm);
+
+  function recibirDatosForm(e) {
+    e.preventDefault();
+    let listaPronombres = readCheckboxValue(avaiblePronoums);
+    let listaAdjetivos = readCheckboxValue(avaibleAdjetives);
+    let listaNombres = readValueFromId("productNames")
+      .replace(/\s/g, "")
+      .split(",");
+    let listaExtensiones = readCheckboxValue(avaibleExtensions);
+
+    let domainListHtml = generateDomainList({
+      pronoums: listaPronombres,
+      adjetives: listaAdjetivos,
+      names: listaNombres,
+      extensions: listaExtensiones
+    });
+
+    if (
+      listaPronombres.length == 0 ||
+      listaAdjetivos.length == 0 ||
+      listaExtensiones.length == 0
+    ) {
+      domainListHtml = ALERT_MISSING_CHECKBOX;
+    }
+    if (listaNombres[0] == "") {
+      domainListHtml = ALERT_MISSING_NAME;
+    }
+    writeToId("domainBox", domainListHtml);
+  }
+
+  let writeToId = (searchedID, strToWrite) => {
+    document.getElementById(searchedID).innerHTML = strToWrite;
+  };
+
+  let readCheckboxValue = listName => {
+    let valueList = [];
+    for (let itemName of listName) {
+      let item = document.getElementById(itemName);
+      if (item.checked) {
+        valueList.push(item.value);
+      }
+    }
+    return valueList;
+  };
+
+  function generateDomainList(objetoForm) {
+    let domainBoxHtml = ``;
+    for (let pronombre of objetoForm.pronoums) {
+      for (let adjetivo of objetoForm.adjetives) {
+        for (let nombre of objetoForm.names) {
+          for (let extension of objetoForm.extensions) {
+            domainBoxHtml += `
+            <p class="my-1 border bg-secondary border-secondary rounded">
+              <a
+                class="text-decoration-none text-light"
+                href="http://${pronombre}${adjetivo}${nombre}${extension}"
+                >
+                ${pronombre}${adjetivo}${nombre}${extension}  
+              </a>
+            </p>        
+          `;
+          }
+        }
+      }
+    }
+    return domainBoxHtml;
+  }
+
+  let readValueFromId = searchedID => document.getElementById(searchedID).value;
+
+  let createCheckForms = (listWanted, wantedID) => {
+    let allChecks = "";
+    for (let avaibleThing of listWanted) {
+      allChecks += `
+      <div>
+        <input
+          class="form-check-input "
+          type="checkbox"
+          value="${avaibleThing}"
+          id="${avaibleThing}"
+        />
+        <label class="form-check-label" for="${avaibleThing}">
+          ${avaibleThing}
+        </label>
+      </div>
+    `;
+    }
+    writeToId(wantedID, allChecks);
+  };
+
+  createCheckForms(avaiblePronoums, "pronoumListForm");
+  createCheckForms(avaibleAdjetives, "adjetivesListForm");
+  createCheckForms(avaibleExtensions, "extensionListForm");
+
+  /// zona de pruebas
+
+  /// FIN -- zona de pruebas -- FIN
 };
